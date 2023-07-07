7607516e9d0dca08def349897da6b734e36b7448
Shariq <shariqnadeem21@hotmail.com>
2023-07-08 01:08:46 +0400

    added initial code for display updating logic

--- calculator.js
+++ calculator.js
@@ -1,7 +1,12 @@
+let display = document.getElementById("calculator-screen");
+let btnWrapper = document.getElementById('noBtnContainer')
 
-var no1;
-var no2;
+
+
+var arg1;
+var arg2;
 var op;
+var result;
 
 //variable storing value currently displayed
 var displayed;
@@ -31,15 +36,61 @@ function operate(no1, no2, op){
     }
 
     else if (op==="-"){
-        return subtract(no1, no2)
+        return subtract(no1, no2);
     }
 
     else if (op==="*"){
-        return multiply(no1, no2)
+        return multiply(no1, no2);
     }
 
     else if (op==="/"){
-        return multiply(no1, no2)
+        return multiply(no1, no2);
     }
 }
 
+function buttonPress() {
+    btnWrapper.addEventListener('click', (event)=> {
+        const isButton = event.target.nodeName === 'BUTTON';
+        if (!isButton) {
+            return;
+        }
+        // return event.target.value;
+        updateDisplay(event.target.value);
+        
+    })
+}
+
+function updateDisplay(button){
+    // do not want display to show equal sign 
+    if (button != "=" && (no1 === null || no2 === null)){
+        display.value += button;
+    }
+}
+    
+
+//updating calculator screen 
+
+// btnWrapper.addEventListener('click', (event) => {
+//   const isButton = event.target.nodeName === 'BUTTON';
+//   if (!isButton) {
+//     return;
+//   }
+//   output.value = event.target.value;
+
+// //   console.dir(event.target.value);
+// })
+
+// function updateInput(){
+//     btnWrapper.addEventListener('click', (event) => {
+//         const isButton = event.target.nodeName === 'BUTTON';
+//         if (!isButton) {
+//           return;
+//         }
+//         output.value = event.target.value;
+//         no1 = parseFloat(event.target.value); 
+      
+//       //   console.dir(event.target.value);
+//     })
+// }
+
+buttonPress();
